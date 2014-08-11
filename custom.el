4816fa1113c69c42ce29e288fac06363aa62a79a
Greg Ichneumon Brown <gbrown5878@gmail.com>
2014-08-10 21:37:36 -0600

    Add my custom.el file to the repo and don't ignore changes to it.

---
+++ custom.el
@@ -0,0 +1,42 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(delete-selection-mode t)
+ '(fci-rule-color "#282a2e")
+ '(safe-local-variable-values (quote ((simplenote-key . "agtzaW1wbGUtbm90ZXIqCxIETm90ZSIgMzVkZjkxNGE1YTBmMTFlM2FlNTU2NzIxOTIwZjhmOWYM") (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby"))))
+ '(session-use-package t nil (session))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#cc6666") (40 . "#de935f") (60 . "#f0c674") (80 . "#b5bd68") (100 . "#8abeb7") (120 . "#81a2be") (140 . "#b294bb") (160 . "#cc6666") (180 . "#de935f") (200 . "#f0c674") (220 . "#b5bd68") (240 . "#8abeb7") (260 . "#81a2be") (280 . "#b294bb") (300 . "#cc6666") (320 . "#de935f") (340 . "#f0c674") (360 . "#b5bd68"))))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:foreground "Green"))))
+ '(diff-removed ((t (:foreground "Red")))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Background coloring
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(set-face-background 'default      "black") ; frame background
+(set-face-foreground 'default      "white") ; normal text
+(set-face-foreground 'isearch      "red")
+
+;; Set cursor and mouse-pointer colours
+(set-cursor-color "red")
+(set-mouse-color "goldenrod")
+
+;; Set region background colour
+;;(set-face-background 'region "blue")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Custom colors for diff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
